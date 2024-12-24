c91500a662767f6a64d3778ad38f4dba970757b9
汪明杰 <1209497940@qq.com>
2024-12-24 09:56:31 +0800

    update

--- src/components/ScoreBoard/ProgressBar.vue
+++ src/components/ScoreBoard/ProgressBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="board">
-    <span>尚未匹配的卡牌对</span>
-    <h2>{{ nonMatchedPairs + 1 }}</h2>
+    <span>尚未翻开的卡牌</span>
+    <h2>{{ 2 * nonMatchedPairs + 1 }}</h2>
   </div>
 </template>
 
